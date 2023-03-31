1f868b3d61c0dfb04323ef3570f556228bdfbd2f
Wagilba <122546245+Wagilba@users.noreply.github.com>
2023-03-31 20:12:39 +0100

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,6 +1,13 @@
 // Define byeSpeaker object
+/*
 var byeSpeaker = {
   speak: function(name) {
     console.log("Goodbye " + name);
   }
 };
+*/
+
+var speakWord = "Good Bye";
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
