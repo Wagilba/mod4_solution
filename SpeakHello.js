b0b0193b2eed84a5a46b6d14eab0d98c26a36ca0
Wagilba <122546245+Wagilba@users.noreply.github.com>
2023-03-31 20:09:24 +0100

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,6 +1,16 @@
 // Define helloSpeaker object
-var helloSpeaker = {
+/*var helloSpeaker = {
   speak: function(name) {
     console.log("Hello " + name);
   }
 };
+*/
+
+(function(window) {	
+	var speakWord = "Hello";
+	var helloSpeaker = {};	
+	helloSpeaker. speak = function (name) {
+		console.log(speakWord + " " + name);
+	}
+	window.helloSpeaker =  helloSpeaker;
+})(window);
